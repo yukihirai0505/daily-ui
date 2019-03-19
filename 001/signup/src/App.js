da3caab52f023ee9448ed714decf0bbcd3c3ede5
Yuki Hirai <yukihirai0505@gmail.com>
2019-03-19 11:48:07 +0900

    Add styles based on props (#001)

--- 001/signup/src/App.js
+++ 001/signup/src/App.js
@@ -6,11 +6,23 @@ const Blog = styled.h1`
   color: skyBlue;
 `
 
+const Button = styled.button`
+  background: ${props => (props.primary ? 'green' : 'white')};
+  color: ${props => (props.primary ? 'white' : 'green')};
+  font-size: 1.5em;
+  padding: 0.25em 1em;
+  border: 2px solid green;
+  border-radius: 5px;
+  margin: 10px;
+`
+
 class App extends Component {
   render() {
     return (
       <div className={this.props.className}>
         <Blog>AppDividend</Blog>
+        <Button primary="styled">Styled</Button>
+        <Button>Plain</Button>
       </div>
     )
   }
