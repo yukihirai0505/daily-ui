bcc6c52e6cf0bb87320090766a80f658c0603bf5
Yuki Hirai <yukihirai0505@gmail.com>
2019-03-19 11:44:02 +0900

    Add global style (#001)

--- 001/signup/src/index.js
+++ 001/signup/src/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createGlobalStyle } from 'styled-components'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const GlobalStyle = createGlobalStyle`
+  body {
+      background: #eddb53;
+      color: white;
+      padding: 0;
+      margin: 0;
+      font-family: cursive;
+  }
+`
+
+ReactDOM.render(
+  <Fragment>
+    <GlobalStyle />
+    <App />
+  </Fragment>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
